a3d277a0bbe651bbe43c4e17d10196ec11a585f2
AltanGuevenBerlin <altan.gueven84@gmail.com>
2025-01-14 14:29:07 +0100

    add allToDos from server (postman)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
-import TodoCard from "./components/TodoCard.tsx";
 import {Todo} from "./Todo.ts";
 import {useEffect, useState} from "react";
 import axios from "axios";
+import TodoColumn from "./components/TodoColumn.tsx";
+import {allPossibleTodos} from "./TodoStatus.ts";
 
 function App() {
 
@@ -22,12 +23,18 @@ function App() {
     }
 
   return (
-    <>
-        <h1>To Do`s</h1>
-        {
-            todos.map(todo => <TodoCard todo={todo} key={todo.id} />)
-        }
-    </>
+      <>
+          <div className="page">
+              <h1>To Do`s</h1>
+              {
+                  allPossibleTodos.map(status => {
+                      const filteredTodos = todos.filter(todo => todo.status === status)
+                      return <TodoColumn status={status} todos={filteredTodos}/>
+                  })
+              }
+          </div>
+
+      </>
   )
 }
 
