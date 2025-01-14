c69db14f1e5fbf5052f15418fb71044f09d33f51
AltanGuevenBerlin <altan.gueven84@gmail.com>
2025-01-14 14:29:07 +0100

    add allToDos from server (postman)

--- frontend/src/Todo.ts
+++ frontend/src/Todo.ts
@@ -1,5 +1,5 @@
 export type Todo = {
     id: string,
     description: string,
-    status: "OPEN" | "IN PROGRESS" | "DONE"
+    status: TodoStatus
 }
---
+++ frontend/src/components/TodoColumn.tsx
@@ -0,0 +1,22 @@
+import TodoCard from "./TodoCard.tsx";
+import {Todo} from "../Todo.ts";
+import {TodoStatus} from "../TodoStatus.ts";
+
+type Props = {
+    status: TodoStatus
+    todos: Todo [],
+
+}
+
+function TodoColumn(props: Props) {
+    return (
+        <div>
+            <h2>{props.status}</h2>
+        {
+            props.todos.map(todo => <TodoCard todo={todo} key={todo.id} />)
+        }
+        </div>
+    );
+}
+
+export default TodoColumn;
